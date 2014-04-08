1a1240343d599134c27990b6b0575bcba32c64ba
zavaboy <zavaboy@gmail.com>
2014-04-07 21:45:11 -0400

    Updated copyright

--- challonge.php
+++ challonge.php
@@ -15,7 +15,7 @@ License: MIT
 /*
 The MIT License (MIT)
 
-Copyright (c) 2013 Ivik Injerd
+Copyright (c) 2013-2014 Ivik Injerd
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
